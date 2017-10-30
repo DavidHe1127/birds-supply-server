0f00100a447f78067a5fdb3e226ee1b680e80b59
David He <jialhe85@gmail.com>
2017-10-30 16:20:04 +1100

    Add json reader/writer utils

--- apis/employees/add.js
+++ apis/employees/add.js
@@ -1,5 +1,24 @@
-const add = () => {
+const fs = require('fs');
 
+import Employee from './index';
+const { fetch } from '../../utils';
+
+const add = ({
+  first_name,
+  last_name,
+  email,
+  gender,
+  company_id,
+  admin,
+  ip_address
+}) => {
+  const employee = new Employee(arguments[0]);
+  data.employees.push(employee);
+
+  return new Promise(resolve, reject) => {
+
+
+  };
 };
 
 export default add;
