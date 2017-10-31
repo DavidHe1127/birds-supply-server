bbb448064d87abba8fdf0b39bf7e5e8736b69c84
David He <jialhe85@gmail.com>
2017-10-31 17:16:09 +1100

    Add employee model

---
+++ server/models/employee.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema({
+
+  email: {
+    type: String,
+    match: [/.+\@.+\..+/, 'Please fill a valid email address']
+  },
+
+  name: {
+    first: {
+      type:String,
+      trim: true,
+      required: true
+    },
+    last {
+      type:String,
+      trim: true,
+      required: true
+    }
+  },
+
+  companyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Company',
+    index: true,
+    require: true
+  },
+
+  gender: {
+    type: String,
+    enum: ['female', 'male']
+  },
+
+  admin: {
+    type: Boolean,
+    default: false
+  },
+
+  ip_address: {
+    type: String
+  }
+
+});
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
