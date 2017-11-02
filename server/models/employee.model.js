07f977d19b4faf36a2bec09d95c45b74c8bc7803
David He <jialhe85@gmail.com>
2017-11-02 23:37:59 +1100

    Bulk load seed data into db

--- server/models/employee.model.js
+++ server/models/employee.model.js
@@ -1,48 +1,48 @@
 const mongoose = require('mongoose');
 
-const EmployeeSchema = new mongoose.Schema({
+const EmployeeSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
 
-  email: {
-    type: String,
-    match: [/.+\@.+\..+/, 'Please fill a valid email address']
-  },
-
-  name: {
-    first: {
-      type:String,
+    first_name: {
+      type: String,
       trim: true,
       required: true
     },
-    last: {
-      type:String,
+
+    last_name: {
+      type: String,
       trim: true,
       required: true
+    },
+
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
+      index: true,
+      require: true
+    },
+
+    gender: {
+      type: String,
+      enum: ['female', 'male']
+    },
+
+    admin: {
+      type: Boolean,
+      default: false
+    },
+
+    ip_address: {
+      type: String
     }
   },
-
-  companyId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Company',
-    index: true,
-    require: true
-  },
-
-  gender: {
-    type: String,
-    enum: ['female', 'male']
-  },
-
-  admin: {
-    type: Boolean,
-    default: false
-  },
-
-  ip_address: {
-    type: String
+  {
+    collection: 'employees'
   }
-
-}, {
-  collection: 'employees'
-});
+);
 
 module.exports = mongoose.model('Employee', EmployeeSchema);
