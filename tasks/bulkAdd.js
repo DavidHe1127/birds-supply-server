6e8836c1c52a2e633798b8d3dec54de6bd7f163b
David He <jialhe85@gmail.com>
2017-11-02 17:07:59 +1100

    WIP buldAdd task

--- tasks/bulkAdd.js
+++ tasks/bulkAdd.js
@@ -1,16 +1,33 @@
+const args = process.argv.slice(2);
+
+if (args.length !== 1) {
+  throw new Error('multiple input files not supported');
+}
+
+const [file] = args;
+
+const { promisify } = require('util');
+const fs = require('fs');
 const connect = require('../server/db/conn');
+const path = require('path');
+const mongoose = require('mongoose');
 
 const Company = require('../server/models/company.model');
 
-async function addCompany() {
-  await connect();
-  const company = new Company({
-    name: "Allen's Pets",
-    address: '29 Keats Ave Rockdale',
-    id: mongoose.Schema.Types.ObjectId
-  });
+const readFile = promisify(fs.readFile);
 
-  const res = await company.save();
+async function doIt(name) {
+  connect();
+  const content = await readFile(path.join(__dirname, 'inputs', name), 'utf8');
+
+  try {
+    const json = JSON.parse(content);
+    json.filter(async x => {
+      x.id = mongoose.Schema.Types.ObjectId;
+      const company = new Company(x);
+      await company.save();
+    });
+  } catch(err) {
+    throw new Error('invalid json input');
+  }
 }
-
-addCompany();
