8382abba02e1f1bc94d61af85715422010ecf856
David He <jialhe85@gmail.com>
2017-11-08 10:48:24 +1100

    Update dependency and mongorc

--- mongorc.js
+++ mongorc.js
@@ -1,7 +1,9 @@
 // save it into ~/.mongorc.js
 // prettify output find().unpretty() to unformat output
-DBQuery.prototype._prettyShell = true
+DBQuery.prototype._prettyShell = true;
 DBQuery.prototype.unpretty = function () {
         this._prettyShell = false;
         return this;
-}
+};
+
+DBQuery.shellBatchSize = 10;
