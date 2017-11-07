9e9cc65807bdc5103b7a61d09b8bc47b1279cab4
David He <jialhe85@gmail.com>
2017-11-08 10:48:54 +1100

    Done employees by company query

--- server/apis/employees/filter.js
+++ server/apis/employees/filter.js
@@ -1,7 +1,5 @@
 const Employees = require('../../models/employee.model');
 
-console.log('asdsadsadsadsadsadasdsa');
-
-const filter = ({ company_id }) => Employees.find({ company: company_id });
+const filter = (company_id) => Employees.find({ company: company_id });
 
 export default filter;
