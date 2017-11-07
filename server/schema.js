353511e70dbe49974f2ea6e31bef497b94fbd6a8
David He <jialhe85@gmail.com>
2017-11-07 17:37:25 +1100

    WIP schema stiching

--- server/schema.js
+++ server/schema.js
@@ -1,6 +1,9 @@
 import { makeExecutableSchema, mergeSchemas } from 'graphql-tools';
-import query from './schemas/query.graphql';
-import mutation from './schemas/mutation.graphql';
+
+import employeeQuery from './schemas/employee/query.graphql';
+import companyQuery from './schemas/company/query.graphql';
+
+import employeeMutation from './schemas/employee/mutation.graphql';
 
 import { resolvers } from './resolvers';
 
@@ -11,6 +14,67 @@ import { resolvers } from './resolvers';
 
 // turns type definitions into an executable schema to which we can add custom resolvers
 // resolvers tell server how to resolve each part of a query
-const schema = makeExecutableSchema({ typeDefs: [mutation, query], resolvers });
+const employeeSchema = makeExecutableSchema({
+  typeDefs: [employeeQuery, employeeMutation]
+});
+
+// const companySchema = makeExecutableSchema({
+//   typeDefs: [companyQuery]
+// });
+
+console.log('xc')
+
+const schema = mergeSchemas({
+  schemas: [employeeSchema, /* companySchema */],
+  // resolvers
+});
 
 export { schema };
+
+// import {
+//   makeExecutableSchema,
+//   addMockFunctionsToSchema,
+//   mergeSchemas,
+// } from 'graphql-tools';
+
+// // Mocked chirp schema; we don't want to worry about the schema
+// // implementation right now since we're just demonstrating
+// // schema stitching
+// const chirpSchema = makeExecutableSchema({
+//   typeDefs: `
+//     type Chirp {
+//       id: ID!
+//       text: String
+//       authorId: ID!
+//     }
+
+//     type Query {
+//       chirpById(id: ID!): Chirp
+//       chirpsByAuthorId(authorId: ID!): [Chirp]
+//     }
+//   `
+// });
+
+// addMockFunctionsToSchema({ schema: chirpSchema });
+
+// // Mocked author schema
+// const authorSchema = makeExecutableSchema({
+//   typeDefs: `
+//     type User {
+//       id: ID!
+//       email: String
+//     }
+
+//     type Query {
+//       userById(id: ID!): User
+//     }
+//   `
+// });
+
+// // This function call adds the mocks to your schema!
+// addMockFunctionsToSchema({ schema: authorSchema });
+// const schema = mergeSchemas({
+//   schemas: [chirpSchema, authorSchema],
+// });
+
+// export { schema };
