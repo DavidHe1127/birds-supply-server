dd3237adf922c9673de241e22206e27f53acaa75
David He <jialhe85@gmail.com>
2017-11-08 17:31:56 +1100

    WIP remove dummy resolvers

--- server/apis/employees/filter.js
+++ server/apis/employees/filter.js
@@ -1,8 +1,5 @@
 const Employees = require('../../models/employee.model');
 
-const filter = company_id => {
-  console.log(company_id)
-  return Employees.find({ company_id });
-}
+const filter = company_id => Employees.find({ company_id });
 
 export default filter;
