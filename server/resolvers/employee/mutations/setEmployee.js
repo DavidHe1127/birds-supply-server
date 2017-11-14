74efc99c139cdc0e67dcb089d924282e45fa578b
David He <jialhe85@gmail.com>
2017-11-14 15:49:38 +1100

    Add setEmployee mutations

--- server/resolvers/employee/mutations/setEmployee.js
+++ server/resolvers/employee/mutations/setEmployee.js
@@ -1,27 +1,59 @@
 const Employee = require('../../../models/employee.model');
+const Company = require('../../../models/company.model');
 
-const Employee = (root, args) => {
-  const {
-    email,
-    admin,
-    company_id,
-    _id
-  } = args.input;
+const setEmployee = async (root, args) => {
+  const { email, admin, company_id, _id } = args.input;
 
+  const oldEmployee = await Employee.findById(_id);
 
+  const updates = {};
 
-  Employee.findByIdAndUpdate(_id, {$set: {email, admin, company_id}})
+  if (email !== oldEmployee.email) {
+    updates.email = email;
+  }
 
+  if (admin !== oldEmployee.admin) {
+    updates.admin = admin;
+  }
 
+  if (!oldEmployee.company_id.equals(company_id)) {
+    updates.company_id = company_id;
+  }
 
+  if (Object.values(updates).length === 0) {
+    return oldEmployee;
+  }
+
+  const employee = await oldEmployee.update(updates);
+
+  if (updates.hasOwnProperty('company_id')) {
+    await Company.update(
+      {
+        _id: oldEmployee.company_id
+      },
+      {
+        $pull: {
+          employees: {
+            _id: _id
+          }
+        }
+      }
+    );
+
+    await Company.update(
+      {
+        _id: company_id
+      },
+      {
+        $push: {
+          employees: employee
+        }
+      }
+    );
+  }
+
+  return employee;
 };
 
-Employee.schema.post('findOneAndUpdate')
+module.exports = setEmployee;
 
-
-Tank.findByIdAndUpdate(id, { $set: { size: 'large' }}, { new: true }, function (err, tank) {
-  if (err) return handleError(err);
-  res.send(tank);
-});
-
-module.exports = addEmployee;
