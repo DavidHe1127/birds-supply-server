fb4c399730f5a5c6e25240faaccf111beafe3050
David He <jialhe85@gmail.com>
2017-11-19 17:11:43 +1100

    Add parrot model

---
+++ server/models/parrot.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const ParrotSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: true
+  },
+
+  size: {
+    type: Number,
+    required: true,
+    enum: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+  },
+
+  mass: {
+    type: Number,
+    required: true
+  },
+
+  range: {
+    type: String,
+    required: true
+  },
+
+  lifespan: {
+    type: Number,
+    required: true
+  },
+
+  species: {
+    type: String,
+    enum: ['macaws', 'conures', 'cockatoos', 'amazons']
+  },
+
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true
+  }
+
+  // parrots: [Parrot.schema]
+
+}, {
+  collection: 'parrots'
+});
+
+module.exports = mongoose.model('Parrot', ParrotSchema);
