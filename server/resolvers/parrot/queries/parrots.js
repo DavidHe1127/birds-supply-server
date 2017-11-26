12ffb42a9642ed1ca341b352db6c801ce71bfb6c
David He <jialhe85@gmail.com>
2017-11-26 17:23:51 +1100

    Fix unworking supplier & parrot queries

--- server/resolvers/parrot/queries/parrots.js
+++ server/resolvers/parrot/queries/parrots.js
@@ -1,5 +1,6 @@
-const Parrots = require('../../../models/customer.model');
+const Parrots = require('../../../models/parrot.model');
+const { connectionFromArray } = require('graphql-relay');
 
-const parrots = obj => Parrots.find({});
+const parrots = (obj, args) => Parrots.find({}).then(res => connectionFromArray(res, args));
 
 module.exports = parrots;
