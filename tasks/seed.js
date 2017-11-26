04811a5aa495f792495b80443263c6e3bd01ef46
David He <jialhe85@gmail.com>
2017-11-26 11:08:02 +1100

    Done seed loader

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,12 +1,30 @@
+const mongoose = require('mongoose');
+
 const connect = require('../server/db/conn');
 const Parrots = require('../server/models/parrot.model');
+const Suppliers = require('../server/models/supplier.model');
 
 const parrots = require('./seeds/parrots.json');
+const suppliers = require('./seeds/suppliers.json');
 
-doIt();
-
-async function doIt () {
+// IIFE
+(async () => {
   connect();
-  const res = await Parrots.insertMany(parrots);
-  console.log(res);
-}
+
+  await mongoose.connection.collections.suppliers.drop();
+  await mongoose.connection.collections.parrots.drop();
+
+  const syncedParrots = await Parrots.insertMany(parrots);
+
+  const suppliersToSync = suppliers.map(s => ({
+    ...s,
+    parrots: s.parrots.map(p => syncedParrots.find(x => x.code === p)._id)
+  }));
+
+  await Suppliers.insertMany(suppliersToSync);
+
+  process.exit();
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
