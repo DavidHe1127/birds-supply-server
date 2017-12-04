d4dd9a06986d9a1c3a6d9dd6910efdcdfebb1f76
David He <jialhe85@gmail.com>
2017-12-04 16:39:14 +1100

    Seed products

--- tasks/seed.js
+++ tasks/seed.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const connect = require('../server/db/conn');
 const Parrots = require('../server/models/parrot.model');
 const Suppliers = require('../server/models/supplier.model');
+const Products = require('../server/models/product.model');
 
 const parrots = require('./seeds/parrots.json');
 const suppliers = require('./seeds/suppliers.json');
+const products = require('./seeds/products.json');
 
 // IIFE
 (async () => {
@@ -13,6 +15,7 @@ const suppliers = require('./seeds/suppliers.json');
 
   await mongoose.connection.collections.suppliers.drop();
   await mongoose.connection.collections.parrots.drop();
+  await mongoose.connection.collections.products.drop();
 
   const syncedParrots = await Parrots.insertMany(parrots);
 
@@ -21,7 +24,15 @@ const suppliers = require('./seeds/suppliers.json');
     parrots: s.parrots.map(p => syncedParrots.find(x => x.code === p)._id)
   }));
 
-  await Suppliers.insertMany(suppliersToSync);
+  const syncedSuppliers = await Suppliers.insertMany(suppliersToSync);
+
+  const productsToSync = products.map(p => ({
+    ...p,
+    parrot: syncedParrots.find(x => x.code === p.parrot)._id,
+    supplier: syncedSuppliers.find(x => x.code === p.supplier)._id
+  }));
+
+  await Products.insertMany(productsToSync);
 
   process.exit();
 })().catch(err => {
