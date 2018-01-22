4d0061045f6358cd3cffd198f8693db81c6fe34b
David He <jialhe85@gmail.com>
2018-01-22 22:17:18 +1100

    Add setProduct mutation

---
+++ server/resolvers/product/mutations/setProduct.js
@@ -0,0 +1,34 @@
+const Products = require('../../../models/product.model');
+
+const setProduct = async (obj, args) => {
+  const { id, qty, price } = args.input;
+  const params = {};
+
+  if (qty) {
+    params.qty = qty;
+  }
+
+  if (price) {
+    params.price = price;
+  }
+
+  const product = await Products.findByIdAndUpdate(id, params, {
+    new: true
+  });
+
+  return {
+    product
+  };
+};
+
+module.exports = setProduct;
+
+// mutation {
+//   addProduct(input: {
+//     price: 777
+//     code: "sulphur-crested_cockatoo"
+//     qty: 77
+//   }) {
+//     id
+//   }
+// }
