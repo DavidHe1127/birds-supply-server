acde1a8773d6c0495899635670f09f232884f3b9
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2018-01-29 09:30:56 +1100

    Fix best sellers query for count field (#4)

--- server/resolvers/queries/bestSellers.js
+++ server/resolvers/queries/bestSellers.js
@@ -1,16 +1,19 @@
 const Products = require('../../models/product.model');
 const { connectionFromArray } = require('graphql-relay');
 
-const bestSellers = (obj, args) =>
-  Products.find({
+const bestSellers = async (obj, args) => {
+  const products = await Products.find({
     qty: {
       $lt: 50
     }
   })
-    .limit(3)
-    .populate('parrot')
-    .populate('supplier')
-    .exec()
-    .then(res => connectionFromArray(res, args));
+  .limit(3)
+  .populate('parrot')
+  .populate('supplier')
+  .exec()
+  .then(res => connectionFromArray(res, args));
+  
+  return Object.assign(products, { count: products.edges.length || 0 });
+}
 
 module.exports = bestSellers;
