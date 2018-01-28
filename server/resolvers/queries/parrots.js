45b6bc27feaeae43c4f85fa551c492a49c8ac425
emmafallancy <emmanuel.fallancy@gmail.com>
2018-01-28 21:18:25 +1100

    Fix count field for parrots resolver

--- server/resolvers/queries/parrots.js
+++ server/resolvers/queries/parrots.js
@@ -1,6 +1,10 @@
 const Parrots = require('../../models/parrot.model');
 const { connectionFromArray } = require('graphql-relay');
 
-const parrots = (obj, args) => Parrots.find({}).then(res => connectionFromArray(res, args));
+const parrots = async (obj, args) => {
+  const parrotsDB = await Parrots.find({}).then(res => connectionFromArray(res, args));
+
+  return Object.assign(parrotsDB, {count: parrotsDB.edges.length || 0});
+}
 
 module.exports = parrots;
