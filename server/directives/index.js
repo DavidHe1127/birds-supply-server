2cb1388bb6fdcf3c4b57a04e5dd6a04e9f5b7704
David He <jialhe85@gmail.com>
2018-02-23 15:34:48 +1100

    Restructure schema

--- server/directives/index.js
+++ server/directives/index.js
@@ -1,46 +1,52 @@
 const {AuthorizationError} = require('../errors');
 const cognito = require('../cognito');
 
-export const isAuthenticated = (next, source, args, context) => {
-  const token = context.headers.authorization;
-  if (!token) {
-    throw new AuthorizationError({
-      message: 'You must supply a JWT for authorization!',
-    });
-  }
-  try {
-    const decoded = cognito.verify(token);
-
-    context.user = decoded;
-    return next();
-  } catch (err) {
-    throw new AuthorizationError({
-      message: 'You are not authorized.',
-    });
-  }
-};
-
-export const hasScope = (next, source, args, context) => {
-  const token = context.headers.authorization;
-  const expectedScopes = args.scope;
-  if (!token) {
-    throw new AuthorizationError({
-      message: 'You must supply a JWT for authorization!',
-    });
-  }
-  try {
-    const decoded = cognito.verify(token);
-    const scopes = decoded.scope.split(' ');
-    if (expectedScopes.some(scope => scopes.indexOf(scope) !== -1)) {
-      return next();
+const directiveResolvers = {
+  isAuthenticated: (next, source, args, context) => {
+    console.log('xcxcxc');
+    const token = context.headers.authorization;
+    if (!token) {
+      throw new AuthorizationError({
+        message: 'You must supply a JWT for authorization!'
+      });
     }
-  } catch (err) {
-    return Promise.reject(
-      new AuthorizationError({
-        message: `You are not authorized. Expected scopes: ${expectedScopes.join(
-          ', ',
-        )}`,
-      }),
-    );
+    try {
+      const decoded = cognito.verify(token);
+
+      context.user = decoded;
+      return next();
+    } catch (err) {
+      throw new AuthorizationError({
+        message: 'You are not authorized.'
+      });
+    }
+  },
+
+  hasScope: (next, source, args, context) => {
+    const token = context.headers.authorization;
+    const expectedScopes = args.scope;
+    if (!token) {
+      throw new AuthorizationError({
+        message: 'You must supply a JWT for authorization!'
+      });
+    }
+    try {
+      const decoded = cognito.verify(token);
+      const scopes = decoded.scope.split(' ');
+      if (expectedScopes.some(scope => scopes.indexOf(scope) !== -1)) {
+        return next();
+      }
+    } catch (err) {
+      return Promise.reject(
+        new AuthorizationError({
+          message: `You are not authorized. Expected scopes: ${expectedScopes.join(
+            ', '
+          )}`
+        })
+      );
+    }
   }
+
 };
+
+export default directiveResolvers;
